c937ba2739555b431e36265d3d9837edc9cd34b4
Riya Mandlikar <108127634+riya09d@users.noreply.github.com>
2024-01-08 16:55:32 +0530

    Add files via upload

---
+++ screenshot.py
@@ -0,0 +1,32 @@
+import time 
+import pyautogui
+import tkinter as tk
+
+def screenshot():
+    name = int(round(time.time() * 1000))
+    name = 'D:/Projects/screenshots/{}.png'.format(name)
+    img = pyautogui.screenshot(name)
+    img.show()
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(
+    frame,
+    text = 'Take Screenshot',
+    command = screenshot
+)
+
+button.pack(side=tk.LEFT)
+close = tk.Button(
+    frame,
+    text = 'Quit',
+    command = quit
+)
+close.pack(side=tk.LEFT)
+
+root.mainloop()
+
+
+
